e3e71bb8789316a54740641ab5b466f75be9b4b6
Luís Lança <luis_lanca@outlook.com>
2019-11-21 14:51:39 -0300

    Add Treeview
    Signed-off-by: Luís Lança <luis_lanca@outlook.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
-// import Paper from './components/Paper';
+import React, { Component } from 'react';
 import TreeView from './components/TreeView';
 import './styles/styles.scss';
 
-function App() {
-  return (
-    <div className="container">
-      {/* <Paper> */}
-        <TreeView />
-      {/* </Paper> */}
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <TreeView />
+      </div>
+    );
+  }
+}
--- src/components/TreeView/index.js
+++ src/components/TreeView/index.js
@@ -1,10 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Tree, Spin } from "antd";
+import api from "../../service/api";
+import "antd/dist/antd.css";
+
+const { TreeNode } = Tree;
 
 export default class TreeView extends Component {
-    render() {
-        return (
-            <>
-            </>
-        );
+  state = {
+    treeData: [],
+    isNull: false
+  };
+
+  // Retorna os dados da requisição
+  async componentDidMount() {
+    const response = await api.get("/db.json");
+    const data = this.formatData(response.data);
+    this.setState({ treeData: data, isNull: true });
+  }
+
+  // Transforma o Objeto JSON em um Array de objetos
+  formatData = data => {
+    let keys = Object.keys(data);
+    let newData = [];
+    keys.forEach(key => {
+      this.logChildContent(data[key]);
+      newData.push(data[key]);
+    });
+    return newData;
+  };
+
+  // Transforma os Objetos filhos em Arrays de objetos
+  logChildContent = child => {
+    let childrenKeys = Object.keys(child.children);
+    let childrenArray = [];
+    if (childrenKeys.length > 0) {
+      childrenKeys.forEach(childKey => {
+        this.logChildContent(child.children[childKey]);
+        childrenArray.push(child.children[childKey]);
+      });
     }
+    child.children = childrenArray;
+  };
+
+  // Renderiza a Treeview
+  renderTreeNodes = data =>
+    data.map(item => {
+      if (item.children) {
+        return (
+          <TreeNode title={item.name} key={item.id} dataRef={item}>
+            {this.renderTreeNodes(item.children)}
+          </TreeNode>
+        );
+      }
+      return <TreeNode key={item.id} {...item} />;
+    });
+
+  render() {
+    const { treeData, isNull } = this.state;
+    return (
+      <div>
+        {!isNull ? (
+          <Spin />
+        ) : (
+          <Tree checkable>{this.renderTreeNodes(treeData)}</Tree>
+        )}
+      </div>
+    );
+  }
 }
